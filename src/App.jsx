c5460b94a279733bc8f12af041134e52ad50d90f
nammtph52069 <nammtph52069>
2024-04-17 04:18:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+function App() {
+  return (
+    <div>
+      <p>nksmkdv</p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default App
